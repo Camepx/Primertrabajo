99bd1a6017d42d05433bb0f9acb7ee5e181c84a3
jujujuana <99354729+jujujuana@users.noreply.github.com>
2022-03-16 18:13:02 -0500

    Bala

--- Assets/Bala.cs
+++ Assets/Bala.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Bala : MonoBehaviour
 {
-    public float velocidad = 2f;
+    public float velocidad=2f;
     // Start is called before the first frame update
     void Start()
     {
