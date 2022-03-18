21d20d0dd01a9fffd09810d382651abc8e2333f7
Camepx <conatio.6@gmail.com>
2022-03-18 14:57:23 -0500

    Script Creado

--- Assets/CarController.cs
+++ Assets/CarController.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarsController : MonoBehaviour
+public class CarController : MonoBehaviour
 {
+    public GameObject 
     // Start is called before the first frame update
     void Start()
     {
