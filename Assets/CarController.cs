47050dd7ff0ee3710e724b7b7e5f0913c3d0b95f
MafeAlvarez <99353716+MafeAlvarez@users.noreply.github.com>
2022-03-18 17:32:03 -0500

    Primera programacion

--- Assets/CarController.cs
+++ Assets/CarController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
     public GameObject micar;
+    public float velocidad = 20f;
+    public GameObject bala;
    
     // Start is called before the first frame update
     void Start()
@@ -15,6 +17,37 @@ public class CarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //transform.position += Vector3.forward * Time.deltaTime; 
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += transform.forward * Time.deltaTime;
+        }
+        //Cuando presione la tecla DOWN el carro avanza hacia atras o en reversa
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position -= transform.forward * Time.deltaTime;
+        }
+        //Cuando presione la tecla RIGCHT
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //transform.position += Vector3.right * Time.deltaTime;
+            transform.eulerAngles += new Vector3(0, velocidad * Time.deltaTime, 0);
+        }
+        //Cuando presione la tecla LEFT
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //transform.position -= Vector3.right * Time.deltaTime;
+            transform.eulerAngles -= new Vector3(0, velocidad * Time.deltaTime, 0);
+        }
+        //Cuando presione la letra A hago un turbo
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += transform.forward * velocidad * Time.deltaTime;
+        }
+        //Cuando presione la letra space hago un disparo
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Instantiate(bala, transform.position, transform.rotation);
+        }
     }
 }
