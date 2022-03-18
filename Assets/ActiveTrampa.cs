23941cbf66e015f3c139804925f1d3779b015cce
MafeAlvarez <99353716+MafeAlvarez@users.noreply.github.com>
2022-03-18 17:47:57 -0500

    Referencia

--- Assets/ActiveTrampa.cs
+++ Assets/ActiveTrampa.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ActiveTrampa : MonoBehaviour
 {
+    public GameObject activarTrampa;
+    public Rigidbody esfera;
     // Start is called before the first frame update
     void Start()
     {
